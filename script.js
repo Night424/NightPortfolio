2629411e7cc7217df0f6ac039a754da6e328f644
Night <keanevgps3@gmail.com>
2025-07-07 19:22:52 +0200

    Removed form

--- script.js
+++ script.js
@@ -285,15 +285,4 @@ function startTypingAnimation() {
       }
     });
   });
-
-  // Form submission
-  const contactForm = document.querySelector('.contact-form');
-  if (contactForm) {
-    contactForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      // Here you would typically send the form data to a server
-      alert('Thank you for your message! I will get back to you soon.');
-      contactForm.reset();
-    });
-  }
 });
